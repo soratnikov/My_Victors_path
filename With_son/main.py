c89b054269fc51aef59b3e972dca000334b8dcf0
soratnikov <soratnikov@gmail.com>
2025-05-05 01:24:28 +0500

    2025

--- With_son/main.py
+++ With_son/main.py
@@ -3,12 +3,12 @@ import pgzrun
 from pgzero.loaders import images
 
 screen: pgzero.screen.Screen
-
+#from pgzero.screen import Screen
 
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 550
 player_x = 500
-player_y = 550
+player_y = 350
 
 def draw():
     screen.blit(images.backdrop, (0,0))
